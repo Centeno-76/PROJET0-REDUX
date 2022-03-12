5e726c6d5bd27d3635dd64ebdd8fe50f220c032c
Centeno-76 <mateuscenteno76@gmail.com>
2022-03-12 15:40:33 +0000

    Início projeto redux

---
+++ projeto-redux/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+import Card from './Components/Card';
+
+function App() {
+  return (
+    <div className='App'>
+
+      <h1>Projeto-Redux</h1>
+      
+      <div className="linha">
+        <Card> </Card>
+
+      </div>
+      <div className="linha">
+        <Card> </Card>
+
+      </div>
+    </div>
+  )
+}
+
+export default App;

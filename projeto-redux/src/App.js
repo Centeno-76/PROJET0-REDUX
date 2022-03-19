c5db32d327a11fa23c07b7b977da00473840d322
Centeno-76 <mateuscenteno76@gmail.com>
2022-03-19 15:39:39 +0000

    Aula 19/03

--- projeto-redux/src/App.js
+++ projeto-redux/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
-
+import Intervalo from './Components/Intervalo';
 import Card from './Components/Card';
+import Media from './Components/Media';
+import Soma from './Components/Soma';
+import Sorteio from './Components/Sorteio';
 
 function App() {
+  const [min, setMin]  = useState(10)
+  const [max, setMax]  = useState(100)
   return (
     <div className='App'>
 
       <h1>Projeto-Redux</h1>
       
       <div className="linha">
-        <Card> </Card>
+        <Intervalo min={min} max={max}></Intervalo>
 
       </div>
       <div className="linha">
-        <Card> </Card>
+       <Media  min={min} max={max}></Media>
+       <Soma min={min} max={max}></Soma>
+       <Sorteio min={min} max={max}></Sorteio>
 
       </div>
     </div>
